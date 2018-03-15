77d91a6e709813881c2332163e666e5a8923d79d
pc710 <pc710@iucesmag.edu.co>
2018-03-15 13:56:03 -0500

    cambio1

--- app/src/main/java/com/example/aula7/quisyojansanchezi056114/MainActivity.java
+++ app/src/main/java/com/example/aula7/quisyojansanchezi056114/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aula7.quisyojansanchezi056114;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opt, menu);
+        return true;
     }
 }
